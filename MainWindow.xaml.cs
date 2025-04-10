803c0cd43707065cbab8b9b245b6726a76820578
Yaroslav <fokrivosheev@mail.ru>
2025-04-10 19:05:54 +0500

    Первая версия простого ввода вывода в окно

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace YaroslavApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = InputBox.Text;
+            OutputText.Text = "Вы ввели: " + text;
+        }
+    }
+}
